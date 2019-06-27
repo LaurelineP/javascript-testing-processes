e201662c131da69d73958306b93fe1e71138d22f
Laureline <laurelinedev@gmail.com>
2019-06-27 17:00:18 +0200

    Added more unit tests: checking another regular behavior like, checking for null or no values + no args passed to a function

--- util.test.js
+++ util.test.js
@@ -1,7 +1,23 @@
 const { generateText } = require('./util');
 
 /** Unit test */
+// 1. Tests regular behavior cases ( double check )
 test('should output name and age', () => {
     const text = generateText('Laureline', 26);
-    expect(text).toBe('Laureline (26 years old)')
+    expect(text).toBe('Laureline (26 years old)');
+
+    const text2 = generateText('Nova', 3);
+    expect(text2).toBe('Nova (3 years old)');
 });
+
+// 2. Tests with empty-data case
+test('should output data-less text', () => {
+    const text = generateText('', null);
+    expect( text ).toBe(' (null years old)');
+})
+
+// 3. Tests with no arguments
+test('should output undefined-value text', () => {
+    const text = generateText();
+    expect( text ).toBe('undefined (undefined years old)');
+})
